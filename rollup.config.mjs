0bbfd90762e0954bd056780ac8befc773d3b23f1
Frans Ekman <fransedvinekman@gmail.com>
2025-06-25 18:25:33 +0200

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,27 @@
+import typescript from "@rollup/plugin-typescript";
+
+const input = "src/index.ts";
+
+/** @type {() => import('rollup').RollupOptions} */
+const createConfig = (format, dir) => ({
+  input,
+  output: {
+    dir,
+    format,
+    sourcemap: true,
+    preserveModules: true,
+  },
+  plugins: [
+    typescript({
+      compilerOptions: {
+        declarationDir: dir,
+        emitDeclarationOnly: true,
+      },
+    }),
+  ],
+});
+
+export default [
+  createConfig("cjs", "dist/cjs"),
+  createConfig("esm", "dist/esm"),
+];
